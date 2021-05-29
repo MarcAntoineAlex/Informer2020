ae0b8a0079041e52be7c1a4c9c5ae4e3c3088f36
MarcAntoineAlex <18510756258@163.com>
2021-05-30 00:29:09 +0800

    c

--- utils/config.py
+++ utils/config.py
@@ -108,7 +108,7 @@ class MInformerConfig(BaseConfig):
 
         parser.add_argument('--w_momentum', type=float, default=0.9)
         parser.add_argument('--w_weight_decay', type=float, default=3e-4)
-        parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
+
 
         args = parser.parse_args()
 
