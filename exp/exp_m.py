7343841537eea8c774973d9b0d9df21efeffcc85
MarcAntoineAlex <18510756258@163.com>
2021-05-30 21:41:02 +0800

    c

--- exp/exp_m.py
+++ exp/exp_m.py
@@ -165,6 +165,10 @@ class Exp_M_Informer(Exp_Basic):
             self.model.train()
             epoch_time = time.time()
             for i, (trn_data, val_data, next_data) in enumerate(zip(train_loader, vali_loader, next_loader)):
+                for t, v, n in zip(trn_data, val_data, next_data):
+                    t.to(self.device)
+                    v.to(self.device)
+                    n.to(self.device)
                 iter_count += 1
                 H_optim.zero_grad()
                 self.arch.unrolled_backward(self.args, trn_data, val_data, next_data, W_optim.param_groups[0]['lr'], W_optim)
