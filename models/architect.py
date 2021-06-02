c2d19a6a0be9e6bac66271c00038b64f74be48f6
MarcAntoineAlex <18510756258@163.com>
2021-06-02 22:53:43 +0800

    c

--- models/architect.py
+++ models/architect.py
@@ -132,10 +132,10 @@ class Architect():
         pred, true = self._process_one_batch(trn_data, self.net)
         loss = self.criterion(pred, true)
         HD = list(self.net.H())
-        HD.append(trn_data[1][:, -self.args.pred_len, :])
+        HD.append(trn_data[1])
         d_wpos = torch.autograd.grad(loss, HD)
         dH_wpos = d_wpos[:-1]
-        dD_wpos = d_wpos[-1]
+        dD_wpos = d_wpos[-1][:, -self.args.pred_len, :]
         dD_wposs = [torch.zeros(dD_wpos.shape).to(self.device) for i in range(args.world_size)]
         dist.all_gather(dD_wposs, dD_wpos)
         if args.rank < args.world_size-1:
