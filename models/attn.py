9e07b9dc44d483083e13a7e52cf9bde6a07b07a6
MarcAntoineAlex <18510756258@163.com>
2021-07-19 21:02:46 +0800

    c

--- models/attn.py
+++ models/attn.py
@@ -137,6 +137,7 @@ class AttentionLayer(nn.Module):
         self.inner_attention = attention
         self.args = args
         print(type(d_model))
+        print(d_model)
         if args is not None:
             self.q_proj = nn.ModuleList()
             self.k_proj = nn.ModuleList()
