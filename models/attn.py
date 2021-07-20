04e441c73547b6fdc1789ac8dda9a6433d9bc798
MarcAntoineAlex <18510756258@163.com>
2021-07-20 19:42:12 +0800

    c

--- models/attn.py
+++ models/attn.py
@@ -142,9 +142,7 @@ class AttentionLayer(nn.Module):
             self.v_proj = nn.ModuleList()
             total = d_keys * n_heads
             w_minus_1 = self.args.world_size - 1
-            L_A = (d_keys * n_heads * self.args.ratio // w_minus_1) * w_minus_1
-            print(type(L_A))
-            print(L_A)
+            L_A = int((d_keys * n_heads * self.args.ratio // w_minus_1) * w_minus_1)
             self.q_proj.append(nn.Linear(d_model, total - L_A))
             self.k_proj.append(nn.Linear(d_model, total - L_A))
             self.v_proj.append(nn.Linear(d_model, total - L_A))
